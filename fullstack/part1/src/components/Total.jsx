2506cfa2355f27f7a7445c2f6a39baa1947e94af
Diego Duarte <duartqx@gmail.com>
2024-01-08 19:16:30 -0300

    fullstack part1 Total component

---
+++ fullstack/part1/src/components/Total.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+/**
+ * @typedef {Object} TotalProps
+ * @property {number} total
+ **/
+
+/**
+ * @param {TotalProps} props
+**/
+const Total = ({ total }) => {
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+export default Total
