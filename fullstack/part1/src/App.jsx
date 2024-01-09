19d5cb12e327be190fd397b7c27fb68b77edf0f3
Diego Duarte <duartqx@gmail.com>
2024-01-08 21:42:32 -0300

    part1d 1.6

--- fullstack/part1/src/App.jsx
+++ fullstack/part1/src/App.jsx
@@ -1,33 +1,68 @@
-import React from "react";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import React, { useState } from "react";
+
+/**
+ * @typedef {Object} ButtonProps
+ * @property {React.MouseEventHandler} handleClick
+ * @property {string} text
+ **/
+
+/**
+ * @param {ButtonProps} props
+ **/
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+/**
+ * @typedef {Object} StatProps
+ * @property {string} text
+ * @property {number} value
+ **/
+
+/**
+ * @param {StatProps} props
+ **/
+const Stat = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        title: "Fundamentals of React",
-        quantity: 10,
-      },
-      {
-        title: "Using props to pass data",
-        quantity: 7,
-      },
-      {
-        title: "State of a component",
-        quantity: 14,
-      },
-    ],
-  };
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    let newGood = good + 1
+    return setGood(newGood)
+  }
+
+  const handleNeutral = () => {
+    let newNeutral = neutral + 1
+    return setNeutral(newNeutral)
+  }
+
+  const handleBad = () => {
+    let newBad = bad + 1
+    return setBad(newBad)
+  }
+
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h2>give feedback</h2>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+
+      <h2>statistics</h2>
+      <Stat text="good" value={good} />
+      <Stat text="neutral" value={neutral} />
+      <Stat text="bad" value={bad} />
     </div>
-  );
+  )
 };
 
 export default App;
