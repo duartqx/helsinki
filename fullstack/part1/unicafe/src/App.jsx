eea0eb4f61d53f1bd297fc6d4eba95af198b1d63
Diego Duarte <duartqx@gmail.com>
2024-01-10 21:04:25 -0300

    fixed anecdotes and unicafe and can now continue working on all three exercizes projects

--- fullstack/part1/unicafe/src/App.jsx
+++ fullstack/part1/unicafe/src/App.jsx
@@ -1,53 +1,63 @@
 import React, { useState } from "react";
+import Statistics from "./components/Statistics";
+import Buttons from "./components/Buttons";
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const mostVoted = votes.indexOf(Math.max(...votes))
+  const all = good + neutral + bad;
+  const avg = (good - bad) / all || 0;
+  const positives = (good * 100) / all || 0;
 
-  const handleSelected = () => {
-    setSelected(() => Math.floor(Math.random() * anecdotes.length))
-  }
-  
-  const handleVote = () => {
-    setVotes(() => {
-      const newVotes = [...votes]
-      newVotes[selected] += 1
-      return newVotes
-    })
-  }
+  let stats = [
+    {
+      text: "good",
+      value: good,
+      handleClick: () => {
+        let newGood = good + 1;
+        return setGood(newGood);
+      },
+    },
+    {
+      text: "neutral",
+      value: neutral,
+      handleClick: () => {
+        let newNeutral = neutral + 1;
+        return setNeutral(newNeutral);
+      },
+    },
+    {
+      text: "bad",
+      value: bad,
+      handleClick: () => {
+        let newBad = bad + 1;
+        return setBad(newBad);
+      },
+    },
+    {
+      text: "all",
+      value: all,
+    },
+    {
+      text: "average",
+      value: avg,
+    },
+    {
+      text: "positives",
+      value: positives,
+    },
+  ];
 
   return (
-    <>
-      <h2>Anecdote of the day</h2>
-      <div>{anecdotes[selected]}</div>
-      <div>has {votes[selected]} votes</div>
-      <div>
-        <button onClick={handleVote}>vote</button>
-        <button onClick={handleSelected}>next anecdote</button>
-      </div>
-
-      {votes[mostVoted] !== 0 && (
-        <>
-          <h2>Anecdote with most votes</h2>
-          <div>{anecdotes[mostVoted]}</div>
-          <div>has {votes[mostVoted]} votes</div>
-        </>
-      )}
-    </>
-  )
+    <div>
+      <h2>give feedback</h2>
+      <Buttons buttons={stats} />
+      <h2>statistics</h2>
+      <Statistics stats={stats} shouldRender={all !== 0} />
+    </div>
+  );
 };
 
 export default App;
