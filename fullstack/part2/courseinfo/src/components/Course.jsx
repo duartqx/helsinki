354a5218174b192681785c434f171be6e1be1643
Diego Duarte <duartqx@gmail.com>
2024-01-13 17:35:26 -0300

    fullstack part2 2.2 and 2.3

--- fullstack/part2/courseinfo/src/components/Course.jsx
+++ fullstack/part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 import * as Types from "../types"
 
 /**
@@ -16,6 +17,7 @@ const Course = ({ course }) => {
     <>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </>
   );
 };
--- fullstack/part2/courseinfo/src/components/Total.jsx
+++ fullstack/part2/courseinfo/src/components/Total.jsx
@@ -1,18 +1,20 @@
-import React from "react"
+import React from "react";
+import * as Types from "../types";
 
 /**
  * @typedef {Object} TotalProps
- * @property {number} total
+ * @property {Types.CourseType} course
  **/
 
 /**
  * @param {TotalProps} props
-**/
-const Total = ({ total }) => {
+ **/
+const Total = ({ course }) => {
+  const total = course.parts
+    .map((p) => p.exercises)
+    .reduce((acc, curr) => acc + curr);
 
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
+  return <p style={{ fontWeight: "bold" }}>total of {total} execises</p>;
+};
 
-export default Total
+export default Total;
