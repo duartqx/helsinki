e56b9db86c0123d78c7d5fe2fea27b2158f17397
Diego Duarte <duartqx@gmail.com>
2024-01-13 19:45:39 -0300

    fullstack part2 2.8

--- fullstack/part2/phonebook/src/App.jsx
+++ fullstack/part2/phonebook/src/App.jsx
@@ -2,22 +2,39 @@ import { useState } from "react";
 import React from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "040-123456789" }]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   /** @param {React.ChangeEvent<HTMLInputElement>} e */
   const handleNewName = (e) => {
     setNewName(e.target.value);
   };
 
+  /** @param {React.ChangeEvent<HTMLInputElement>} e */
+  const handleNewPhone = (e) => {
+    setNewPhone(e.target.value);
+  };
+
   /** @type {React.FormEventHandler} */
   const handleForm = (e) => {
     e.preventDefault();
-    if (persons.filter((p) => p.name === newName).length) {
-      alert(`${newName} is already added to phonebook`)
+
+    if (!newName) {
+      alert("You need to specify a name!")
       return
     }
-    setPersons(persons.concat({ name: newName }));
+
+    if (!newPhone) {
+      alert("You need to specify a phone!")
+      return
+    }
+
+    if (persons.filter((p) => p.name === newName).length) {
+      alert(`${newName} is already added to phonebook!`)
+      return
+    }
+    setPersons(persons.concat({ name: newName, phone: newPhone }));
     setNewName("");
   };
 
@@ -29,11 +46,14 @@ const App = () => {
           name: <input type="text" value={newName} onChange={handleNewName} />
         </div>
         <div>
+          number: <input type="text" value={newPhone} onChange={handleNewPhone} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p) => <p>{p.name}</p>)}
+      {persons.map((p) => <p>{p.name} {p.phone}</p>)}
     </div>
   );
 };
