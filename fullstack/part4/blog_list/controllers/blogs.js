cf8fafeee8c5392ca5ecb15022b1a7cbd2699d2e
Diego Duarte <duartqx@gmail.com>
2024-04-14 18:43:09 -0300

    part4 4.9

--- fullstack/part4/blog_list/controllers/blogs.js
+++ fullstack/part4/blog_list/controllers/blogs.js
@@ -4,12 +4,20 @@ import { Blog } from "../models/blog.js";
 const blogRouter = Router();
 
 blogRouter.get("/", async (_, response) => {
-  response.json(await Blog.find({}));
+  response.json(await Blog.find({})).end();
+});
+
+blogRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog?.id) {
+    return response.status(200).json(blog).end();
+  }
+  response.status(404).end();
 });
 
 blogRouter.post("/", async (request, response) => {
   const blog = await new Blog(request.body).save();
-  response.status(201).json(blog);
+  response.status(201).json(blog).end();
 });
 
 export default blogRouter;
--- fullstack/part4/blog_list/tests/blogs.test.js
+++ fullstack/part4/blog_list/tests/blogs.test.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import app from "../app.js";
 import listHelper from "../utils/list_helper.js";
 import { Blog } from "../models/blog.js";
+import { info } from "node:console";
 
 const api = supertest(app);
 
@@ -31,6 +32,39 @@ describe("api integration tests", () => {
     const response = await api.get("/api/blogs");
     const titles = response.body.map((blog) => blog.title);
     assert(titles.includes("Second blog test"));
+  });
+
+  test("get blog by id", async () => {
+    const response = await api.get("/api/blogs");
+
+    assert.notStrictEqual(
+      response.body,
+      undefined,
+      "response body should not be undefined",
+    );
+    assert.notStrictEqual(
+      response.body,
+      null,
+      "response body should not be null",
+    );
+    assert.notDeepStrictEqual(
+      response.body,
+      [],
+      "response body should not be an empty array",
+    );
+
+    const firstBlog = response.body[0];
+
+    assert.notStrictEqual(firstBlog.id, undefined);
+
+    const result = await api
+      .get(`/api/blogs/${firstBlog.id}`)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    info(result.body);
+
+    assert.deepStrictEqual(firstBlog, result.body);
   });
 });
 
